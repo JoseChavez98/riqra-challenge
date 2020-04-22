ee8f08f8a36f6d99946a7ed0d6519f1864bafd1c
JoseChavez98 <jose.chavez@utec.edu.pe>
2020-04-22 00:06:57 -0500

    setting up web-app

---
+++ src/web-app/riqra-app/src/components/Container/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrap = styled.div`
+    background: #f7f7f9;
+    position = absolute;
+    width: 1024px;
+    height: 656px;
+    margin: auto;
+
+`;
